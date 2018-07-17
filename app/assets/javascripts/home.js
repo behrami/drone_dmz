48645f1c1a8275721f2a50d0ced2cda7d4e59dd9
behrami <behram1993@gmail.com>
2018-07-17 15:21:57 -0400

    changed picutre and close position

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("mySidenav").style.width = "350px";
     document.getElementById("main").style.marginLeft = "250px";
 }
 
