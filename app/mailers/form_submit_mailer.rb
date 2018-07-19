4b38f7756901fad02c0a475ac06b33cbfda33f5b
behrami <behram1993@gmail.com>
2018-07-18 23:36:08 -0400

    changed email again

--- app/mailers/form_submit_mailer.rb
+++ app/mailers/form_submit_mailer.rb
@@ -1,6 +1,6 @@
 class FormSubmitMailer < ApplicationMailer
   def notify_form_submit(user)
     @user = user
-    mail(to: ['yawarabbas042@gmail.com', 'behram1993@gmail.com'], subject: "Drone Notification: #{@user.first_name} submitted applilcation as a #{@user.acc_type}")
+    mail(to: ENV['gmail_username'], subject: "Drone Notification: #{@user.first_name} submitted applilcation as a #{@user.acc_type}")
   end
 end
