d75b8f14dabef5b526690a942ebf93bed0cd667c
behrami <behram1993@gmail.com>
2018-07-18 20:12:22 -0400

    added yawar email

--- app/mailers/form_submit_mailer.rb
+++ app/mailers/form_submit_mailer.rb
@@ -1,6 +1,7 @@
 class FormSubmitMailer < ApplicationMailer
   def notify_form_submit(user)
     @user = user
+    mail(to: 'yawarabbas042@gmail.com ', subject: "Drone Notification: #{ @user.first_name} submitted applilcation as a #{@user.acc_type}")
     mail(to: 'behram1993@gmail.com', subject: "Drone Notification: #{ @user.first_name} submitted applilcation as a #{@user.acc_type}")
   end
 end
